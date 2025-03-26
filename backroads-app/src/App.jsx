73f1cbc1d326a4dadf69a271b72b54168e21cbe3
Jani Kangas <jani.kangas@windowslive.com>
2025-03-26 22:29:21 +0200

    link components

--- backroads-app/src/App.jsx
+++ backroads-app/src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
+import Tours from './components/Tours'
 import Footer from './components/Footer'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Tours />
       <Footer />
     </>
   )
