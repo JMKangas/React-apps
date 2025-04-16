47424e9e1c7e27dab20e2e6cf414caef7bef9cd2
Jani Kangas <jani.kangas@windowslive.com>
2025-04-16 20:12:27 +0300

    Adding more components and data arrays

--- backroads-app/src/data.jsx
+++ backroads-app/src/data.jsx
@@ -1,12 +1,109 @@
+import tour1 from './assets/images/tour-1.jpeg'
+import tour2 from './assets/images/tour-2.jpeg'
+import tour3 from './assets/images/tour-3.jpeg'
+import tour4 from './assets/images/tour-4.jpeg'
+import tour5 from './assets/images/tour-5.jpeg'
+import tour6 from './assets/images/tour-6.jpeg'
+
+//#region pageLinks
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
   { id: 3, href: '#services', text: 'services' },
   { id: 4, href: '#tours', text: 'tours' },
 ]
+//#endregion
 
+//#region social links
 export const socialLinks = [
   { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
   { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
   { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
 ]
+//#endregion
+
+//#region services
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: 'jeLorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.e',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'jeLorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.e',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'jeLorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.e',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2020',
+    title: 'jeLorem ipsum dolor sit amet consectetur',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'Finland',
+    duration: '6',
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'august 26th, 2020',
+    title: 'jeLorem ipsum dolor sit amet consectetur',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'Finland',
+    duration: '6',
+    cost: 2100,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'august 26th, 2020',
+    title: 'jeLorem ipsum dolor sit amet consectetur',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'Finland',
+    duration: '6',
+    cost: 2100,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'august 26th, 2020',
+    title: 'jeLorem ipsum dolor sit amet consectetur',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'Finland',
+    duration: '6',
+    cost: 2100,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: 'august 26th, 2020',
+    title: 'jeLorem ipsum dolor sit amet consectetur',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'Finland',
+    duration: '6',
+    cost: 2100,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: 'august 26th, 2020',
+    title: 'jeLorem ipsum dolor sit amet consectetur',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'Finland',
+    duration: '6',
+    cost: 2100,
+  },
+]
+//#endregion
