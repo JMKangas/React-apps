39add90d3542d111728031ba5ad0d3a3128f9a9a
JMKangas <jmkangas@protonmail.com>
2025-09-20 23:26:32 +0300

    useState and hooks

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,10 +1,12 @@
-import Starter from './tutorial/01-useState/starter/02-useState-basics'
-import Error from './tutorial/01-useState/starter/01-error-example'
+import Starter from './tutorial/01-useState/starter/02-useState-basics';
+import StateArray from './tutorial/01-useState/starter/03-useState-array';
+import Error from './tutorial/01-useState/starter/01-error-example';
 
 function App() {
   return (
     <div className='container'>
       <Starter />
+      <StateArray />
       {/*<h2>Advanced React</h2>*/}
     </div>
   );
--- advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+import React from 'react';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) =>{
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+
+  }
+  const removeAllItems = () => {
+    setPeople([]);
+  }
+
+  return (
+    <div>
+            {people.map((person) =>{
+          const {id, name} = person
+          return <div key={id} className='item'>
+            <h4 style={{ marginTop: '2rem' }}>{name}</h4>
+            <button
+            className='btn'
+            onClick={() => removeItem(id)}>
+              remove person {person.name}
+            </button>
+          </div>
+        })
+      }
+      <button
+      style={{ marginTop: '2rem' }}
+      className='btn'
+      onClick={removeAllItems}>
+        Remove all people
+      </button>
+    </div>
+)
+
 };
 
 export default UseStateArray;
