9b89f64eebb92f6c831caaf721a7975a9be66247
JMKangas <jmkangas@protonmail.com>
2025-09-21 20:56:09 +0300

    changes

--- advanced-react/src/tutorial/01-useState/final/02-useState-basics.jsx
+++ advanced-react/src/tutorial/01-useState/final/02-useState-basics.jsx
@@ -1,18 +1,10 @@
 import { useState } from 'react';
 
 const UseStateBasics = () => {
-  // console.log(useState());
-  // console.log(useState('jo koy'));
-  // const value = useState()[0];
-  // const handler = useState()[1];
-  // console.log(value, handler);
 
   const [count, setCount] = useState(0);
   const handleClick = () => {
-    // console.log(count)
     setCount(count + 1);
-    // be careful, we can set any value
-    // setCount('pants');
   };
   return (
     <div>
